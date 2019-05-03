e8ebc0a25950cf0138716a78be09448b8a9de655
gmuthu17 <gmuthu@berkeley.edu>
2019-05-02 19:40:38 -0700

    added testing script

---
+++ binomial/tests/testScript.R
@@ -0,0 +1,8 @@
+# Testing Script
+# Author: Ganesh Muthu
+# Date: 2 May 2019
+# Description: Loads all required libraries to test binomial package
+
+library(testthat)
+library(binomial)
+devtools::test()
